66c56fcf10e4d6111a5caf01739667ae0674b7c0
nehagarg-123 <nehagarg6201@gmail.com>
2026-06-13 21:41:01 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.CLIENT_URL || '/api',
+  baseURL: process.env.REACT_APP_API_URL || '/api',
   withCredentials: true,
   timeout: 20000,
 });
