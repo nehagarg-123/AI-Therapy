d884524853c601732532daa66b1a3a969d460d26
nehagarg-123 <nehagarg6201@gmail.com>
2026-06-13 23:04:32 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ai-therapy-backend-b0xm.onrender.com/api',
+  baseURL: `${process.env.REACT_APP_API_URL}/api`
   withCredentials: true,
   timeout: 20000,
 });
